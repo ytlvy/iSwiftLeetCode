094c97ecf3411edc707ec99dac5dc91ea8d19e92
gyj <yanjie.guo@kuwo.cn>
2022-01-07 18:57:20 +0800

    Initial Commit

---
+++ iSwiftLeetCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iSwiftLeetCode
+//
+//  Created by ytlvy on 2022/1/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iSwiftLeetCodeUITests/iSwiftLeetCodeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iSwiftLeetCodeUITestsLaunchTests.swift
+//  iSwiftLeetCodeUITests
+//
+//  Created by ytlvy on 2022/1/7.
+//
+
+import XCTest
+
+class iSwiftLeetCodeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
